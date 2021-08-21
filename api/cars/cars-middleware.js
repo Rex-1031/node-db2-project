756e904d4aa463cdd9db13d4dceb7c64331c1ba0
Steve Clair <64785141+Rex-1031@users.noreply.github.com>
2021-08-21 10:20:17 -0400

    cars-middleware.js built

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,19 +1,67 @@
 const Cars = require('./cars-model')
+const vinValidator = require('vin-validator')
 const db = require('../../data/db-config')
-const checkCarId = (req, res, next) => {
 
+
+
+
+const checkCarId = async(req, res, next) => {
+  try{
+    const carId = await Cars.getById(req.param.id)
+    if(!carId){
+      res.status(404).json({
+        message: `car with id ${carId} is not found`
+      })
+    }else{
+      next()
+    }
+  }
+  catch(err){
+    next(err)
+  }
 }
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  const {vin, make, model, mileage, title, transmission} = req.body
+  if(!vin || !make || !model ||!mileage || !title || !transmission){
+    res.status(400).json({
+      message: `${req.body} is missing`
+    })
+  }else{
+    next()
+  }
 }
 
-const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkVinNumberValid = async (req, res, next) => {
+  try{
+    const carVinNumber = await db('cars').where('vin', req.body.vin)
+    const isValidVin = vinValidator.validate(carVinNumber)
+    if(isValidVin === false){
+      res.status(400).json({
+        message: `vin ${carVinNumber} is invalid `
+      })
+    }else{
+      next()
+    }
+  }
+  catch(err){
+    next(err)
+  }
 }
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkVinNumberUnique = async (req, res, next) => {
+  try{
+    const carVinNumber = await db('cars').where('vin', req.body.vin)
+    if(carVinNumber){
+      res.status(400).json({
+        message: `vin ${carVinNumber} already exists`
+      })
+    }
+    else(next)
+  }
+  catch(err){
+    next(err)
+  }
 }
 
 module.exports={
