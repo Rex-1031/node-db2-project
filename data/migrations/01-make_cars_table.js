c2046fbc6baa8ecf8c8ccad040b91a68d938d5f2
Steve Clair <64785141+Rex-1031@users.noreply.github.com>
2021-08-21 11:14:38 -0400

    seeds file added

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable("cars", tbl =>{
+    tbl.increments()
+    tbl.text("vin").unique().notNullable()
+    tbl.text("make")
+    tbl.text("model")
+    tbl.integer("milage")
+    tbl.text("transmission")
+
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists("cars")
 };
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,29 @@
+
 // STRETCH
+exports.seed = async function(knex){
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
+const cars =[
+    {
+        vin: '11111111111111111',
+        make: 'Toyota',
+        model: 'Rav 4',
+        mileage: 84000,
+        title: 'clean',
+        transmission: 'automatic'
+    },
+    {
+        vin: '22222222222222222',
+        make: 'Scion',
+        model: 'Xb',
+        mileage: 120000,
+        title: 'clean',
+    },
+    {
+        vin: '33333333333333333',
+        make: 'Toyota',
+        model: 'Prius',
+        mileage: 50000
+    },
+]
